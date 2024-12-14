6a58cba8304b56e0deb4150ea9cbfe4513ea5607
anonymous-dyce <shauryasinghus@gmail.com>
2024-12-14 11:33:38 -0800

    created binary_testing database in user management

--- main.py
+++ main.py
@@ -23,14 +23,15 @@ from api.channel import channel_api
 from api.group import group_api
 from api.section import section_api
 from api.nestPost import nestPost_api # Justin added this, custom format for his website
+from api.btest1 import btest1_api
 from api.messages_api import messages_api # Adi added this, messages for his website
 from api.carphoto import car_api
 from api.carChat import car_chat_api
-
 from api.vote import vote_api
 from api.student import student_api
 # database Initialization functions
 from model.carChat import CarChat
+from model.btest1 import btest1
 from model.user import User, initUsers
 from model.section import Section, initSections
 from model.group import Group, initGroups
@@ -55,6 +56,7 @@ app.register_blueprint(nestImg_api)
 app.register_blueprint(vote_api)
 app.register_blueprint(car_api)
 app.register_blueprint(student_api)
+app.register_blueprint(btest1_api)
 
 # Tell Flask-Login the view function name of your login route
 login_manager.login_view = "login"
