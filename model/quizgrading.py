daaf0be81bacfa5ad44048686679de265baa2987
179046577 <rchavda2009@gmail.com>
2024-12-16 14:49:37 -0800

    sdsd

--- model/quizgrading.py
+++ model/quizgrading.py
@@ -4,9 +4,9 @@ from __init__ import app, db
 from model.user import User
 from model.group import Group
 
-class NestPost(db.Model):
+class QuizGrading(db.Model):
     """
-    NestPost Model
+    QuizGrading Model
     
     The Post class represents an individual contribution or discussion within a group.
     
@@ -18,7 +18,7 @@ class NestPost(db.Model):
         _group_id (db.Column): An integer representing the group to which the post belongs.
         _image_url (db.Column): A string representing the url path to the image contained in the post
     """
-    __tablename__ = 'nestPosts'
+    __tablename__ = 'quizGrading'
 
     id = db.Column(db.Integer, primary_key=True)
     _title = db.Column(db.String(255), nullable=False)
@@ -127,7 +127,7 @@ class NestPost(db.Model):
             db.session.rollback()
             raise e
 
-def initNestPosts():
+def initQuizGrading():
     """
     The initPosts function creates the Post table and adds tester data to the table.
     
@@ -145,10 +145,10 @@ def initNestPosts():
         db.create_all()
         """Tester data for table"""
         
-        p1 = NestPost(title='Calculus Help', content='Need help with derivatives.', user_id=1, group_id=1, image_url="toby1.png")  
-        p2 = NestPost(title='Game Day', content='Who is coming to the game?', user_id=2, group_id=2, image_url="toby2.png")
-        p3 = NestPost(title='New Releases', content='What movies are you excited for?', user_id=3, group_id=3, image_url="toby3.png")
-        p4 = NestPost(title='Study Group', content='Meeting at the library.', user_id=1, group_id=1, image_url="toby4.png")
+        p1 = QuizGrading(title='Calculus Help', content='Need help with derivatives.', user_id=1, group_id=1, image_url="toby1.png")  
+        p2 = QuizGrading(title='Game Day', content='Who is coming to the game?', user_id=2, group_id=2, image_url="toby2.png")
+        p3 = QuizGrading(title='New Releases', content='What movies are you excited for?', user_id=3, group_id=3, image_url="toby3.png")
+        p4 = QuizGrading(title='Study Group', content='Meeting at the library.', user_id=1, group_id=1, image_url="toby4.png")
         
         for post in [p1, p2, p3, p4]:
             try:
