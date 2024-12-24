7e59afda13044a04b6755fd9adc2843f5ae9f14a
anonymous-dyce <shauryasinghus@gmail.com>
2024-12-23 20:27:57 -0800

    added quizquestions to database

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from api.group import group_api
 from api.section import section_api
 from api.nestPost import nestPost_api # Justin added this, custom format for his website
 from api.quizgrading import quizgrading_api
+from api.quizquestions import quizquestions_api
 from api.messages_api import messages_api # Adi added this, messages for his website
 from api.newquizcreation import quizcreation
 # New API's being tested
@@ -32,6 +33,7 @@ from api.vote import vote_api
 # database Initialization functions
 from model.carChat import CarChat
 from model.quizgrading import initquizgrading
+from model.quizquestions import initquizquestions
 from model.user import User, initUsers
 from model.section import Section, initSections
 from model.group import Group, initGroups
@@ -54,7 +56,6 @@ app.register_blueprint(section_api)
 app.register_blueprint(binaryLearningGameScores_api)
 app.register_blueprint(student_api)
 app.register_blueprint(quizgrading_api)
-app.register_blueprint(quizcreation)
 
 # Tell Flask-Login the view function name of your login route
 login_manager.login_view = "login"
