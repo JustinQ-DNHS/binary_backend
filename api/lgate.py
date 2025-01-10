684fa9e18b6d27969e21a174d137480ac10ad6c6
LarsLindain <andre.lindain@gmail.com>
2025-01-10 00:00:42 -0800

    api test test

--- api/lgate.py
+++ api/lgate.py
@@ -1,56 +1,92 @@
 import jwt
 from flask import Blueprint, request, jsonify, current_app, Response, g
-from flask_restful import Api, Resource 
+from flask_restful import Api, Resource  # used for REST API building
 from datetime import datetime
 from __init__ import app
 from api.jwt_authorize import token_required
-from model.nestPost import NestPost  
+from model.nestPost import NestPost
 
-lgate_api = Blueprint('lgate_api', __name__, url_prefix='/api')
+"""
+This Blueprint object is used to define APIs for the Post model.
+- Blueprint is used to modularize application files.
+- This Blueprint is registered to the Flask app in main.py.
+"""
+lgate = Blueprint('lgate', __name__, url_prefix='/api')
 
+"""
+The Api object is connected to the Blueprint object to define the API endpoints.
+- The API object is used to add resources to the API.
+- The objects added are mapped to code that contains the actions for the API.
+- For more information, refer to the API docs: https://flask-restful.readthedocs.io/en/latest/api.html
+"""
+api = Api(lgate)
 
-api = Api(lgate_api)
-
-class GroupAPI:
+class lgateAPI:
     """
-    Define the API CRUD endpoints for the Group model (e.g., lgate quiz results).
-    There are two operations defined here: 
-    - post: to save the quiz results
-    - get: to retrieve quiz results for the authenticated user
+    Define the API CRUD endpoints for the Post model.
+    There are four operations that correspond to common HTTP methods:
+    - post: create a new post
+    - get: read posts
+    - put: update a post
+    - delete: delete a post
     """
-
     class _CRUD(Resource):
         @token_required()
         def post(self):
-            """
-            Save quiz results from the frontend to the database.
-            """
+            # Obtain the current user from the token required setting in the global context
             current_user = g.current_user
-            data = request.get_json() 
+            # Obtain the request data sent by the RESTful client API
+            data = request.get_json()
+            # Create a new post object using the data from the request
+            post = NestPost(data['name'], data['score'], current_user.id, data['group_id'], data['image_url'])
+            # Save the post object using the Object Relational Mapper (ORM) method defined in the model
+            post.create()
+            # Return response to the client in JSON format, converting Python dictionaries to JSON format
+            return jsonify(post.read())
 
-            if 'name' not in data or 'score' not in data:
-                return jsonify({"error": "Missing required fields: 'name' and 'score'"}), 400
-
-            lgquiz = NestPost(
-                user_id=current_user.id,
-                name=data['name'],
-                score=data['score']
-            )
-
-            try:
-                lgquiz.create() 
-                return jsonify(lgquiz.read()), 201 
-            except Exception as e:
-                return jsonify({"error": str(e)}), 500
-
-        @token_required()  
+        @token_required()
         def get(self):
-            """
-            Retrieve all quiz results for the authenticated user.
-            """
+            # Obtain the current user
             current_user = g.current_user
-            results = NestPost.query.filter_by(user_id=current_user.id).all() 
+            # Find all the posts by the current user
+            posts = NestPost.query.filter(NestPost._user_id == current_user.id).all()
+            # Prepare a JSON list of all the posts, uses for loop shortcut called list comprehension
+            json_ready = [post.read() for post in posts]
+            # Return a JSON list, converting Python dictionaries to JSON format
+            return jsonify(json_ready)
 
-            return jsonify([result.read() for result in results])
+        @token_required()
+        def put(self):
+            # Obtain the current user
+            current_user = g.current_user
+            # Obtain the request data
+            data = request.get_json()
+            # Find the current post from the database table(s)
+            post = NestPost.query.get(data['id'])
+            # Update the post
+            post._name = data['name']
+            post._score = data['score']
+            # Save the post
+            post.update()
+            # Return response
+            return jsonify(post.read())
 
-    api.add_resource(_CRUD, '/lgate')
+        @token_required()
+        def delete(self):
+            # Obtain the current user
+            current_user = g.current_user
+            # Obtain the request data
+            data = request.get_json()
+            # Find the current post from the database table(s)
+            post = NestPost.query.get(data['id'])
+            # Delete the post using the ORM method defined in the model
+            post.delete()
+            # Return response
+            return jsonify({"message": "Post deleted"})
+
+    """
+    Map the _CRUD class to the API endpoints for /post.
+    - The API resource class inherits from flask_restful.Resource.
+    - The _CRUD class defines the HTTP methods for the API.
+    """
+    api.add_resource(_CRUD, '/lagate')
