f0d71898565507fbc9a8d24e377774ea0d41b7dd
anonymous-dyce <shauryasinghus@gmail.com>
2025-01-19 17:26:15 -0800

    jjiiin

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from api.messages_api import messages_api # Adi added this, messages for his web
 from api.general import general_api
 from api.binaryLearningGame import binaryLearningGameScores_api
 from api.student import student_api
-from api.binaryConverter import binaryConverter_api
+from api.binaryConverter import binary_converter_api
 from api.vote import vote_api
 # database Initialization functions
 from model.carChat import CarChat
@@ -55,7 +55,7 @@ app.register_blueprint(binary_history_api)
 # apis under development
 app.register_blueprint(binaryLearningGameScores_api)
 app.register_blueprint(student_api)
-app.register_blueprint(binaryConverter_api)
+app.register_blueprint(binary_converter_api)
 # Tell Flask-Login the view function name of your login route
 login_manager.login_view = "login"
 
